e0634fb30361140b40f7c8db1db3f2cb2c73b419
victor DG <v.degermain@gmail.com>
2022-06-29 18:56:45 +0200

    quete dotenv ok

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express=requre('express')
+const express=require('express');
+const dotenv=require('dotenv').config();
+console.log(process.env.MY_NAME)
 
 const app = express();
 const port = 3000;
 app.listen(port, err => err ? console.error('error server') : console.log(`Server running on port ${port}`))
 
-
+app.get('/', (req, res) => res.status(200).send(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`))
